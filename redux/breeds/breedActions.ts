e6fe6020c3676cfb8c71bef9f07bec01ac5d15f5
Kevin <moturikev@gmail.com>
2022-07-04 16:35:20 +0300

    Minor Bug fix

--- redux/breeds/breedActions.ts
+++ redux/breeds/breedActions.ts
@@ -2,7 +2,7 @@ import { AppThunk } from '../store';
 import instance from '../../utils/instance';
 import { setBreedsData } from './breedSlice'
 
-export const fetchBreedsData = (): AppThunk => async dispatch => {
+export const fetchBreedsData: any = (): AppThunk => async dispatch => {
     let response = await instance.get('breeds?limit=20');
     dispatch(setBreedsData(response?.data));
 };
